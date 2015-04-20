6213d8f6af17f7dcd7ca43dfa4590bd4bbd00504
Lucas <godkiller_nt@hammermaps.de>
2015-04-20 18:24:54 +0200

    Fix PHPFastCache encode() & decode()

--- inc/phpfastcache/phpfastcache.php
+++ inc/phpfastcache/phpfastcache.php
@@ -478,41 +478,28 @@ class phpFastCache {
         return $os;
     }
 
-    function encode($data='',$base64=false) {
-        return $data; 
-        /*
-        $data = serialize($data);
-        if(function_exists('mcrypt_encrypt')) {
-            $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
-            $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
-            $decrypttext = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, 'mcr_'.$this->option['securityKey_mcrypt'], $data, MCRYPT_MODE_ECB, $iv);
-            if($base64) $decrypttext = base64_encode($decrypttext);
-            return $decrypttext;
+    function encode($data) {
+        $write_data = array();
+        if(is_array($data)) {
+            $write_data['array'] = true;
+            $write_data['data'] = serialize($data);
+        } else {
+            $write_data['array'] = false;
+            $write_data['data'] = utf8_encode($data);
         }
-        else
-            return base64_encode($data);
-         */
+
+        return serialize($write_data);
     }
 
-    function decode($value,$base64=false) {
-        return $value;
-        /*
-        if(function_exists('mcrypt_decrypt')) {
-            if($base64) $value = base64_decode($value);
-            $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
-            $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
-            $decrypttext = mcrypt_decrypt(MCRYPT_RIJNDAEL_256, 'mcr_'.$this->option['securityKey_mcrypt'], $value, MCRYPT_MODE_ECB, $iv);
-        }
-        else
-            $decrypttext = base64_decode($value);
-
-       $x = @unserialize(trim($decrypttext));
-       if($x == false) {
-            return $value;
+    function decode($data) {
+        $data = unserialize($data);
+        if($data['array']) {
+            $data = unserialize($data['data']);
         } else {
-            return $x;
+            $data = utf8_decode($data['data']);
         }
-         */
+        
+        return $data;
     }
 
     /*
