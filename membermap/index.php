57a2b1c9880c89fff040b5ac9aa77d7c164df22b
Lucas <godkiller_nt@hammermaps.de>
2015-05-14 17:18:47 +0200

    Fix qry

--- membermap/index.php
+++ membermap/index.php
@@ -55,7 +55,7 @@ while($mm_get = _fetch($mm_qry)) {
     $i++;
 }
 
-$mm_qry = db('SELECT u.`id`, u.`nick`, u.`city` FROM ' .  $db['users'] . ' u WHERE u.`gmaps_koord` != "" AND u.`level` > ' . $level . ' ORDER BY u.gmaps_koord, u.id LIMIT '.($page - 1)*config('m_membermap').','.config('m_membermap'));
+$mm_qry = db('SELECT user.`id`, user.`nick`, user.`city` FROM ' .  $db['users'] . ' as user WHERE user.`gmaps_koord` != "" AND user.`level` > '.$level.' ORDER BY user.gmaps_koord, user.id LIMIT '.($page - 1)*config('m_membermap').','.config('m_membermap'));
 while($mm_user_get = _fetch($mm_qry)) {
     $class = ($color % 2) ? "contentMainSecond" : "contentMainFirst"; $color++;
     $mm_users .= show($dir.'/membermap_users',array('id' => $mm_user_get['id'],
