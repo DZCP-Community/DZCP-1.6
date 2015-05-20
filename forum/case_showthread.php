0a30c78a07da19eb0c7746b871a90493b98f04ad
Lucas <godkiller_nt@hammermaps.de>
2015-05-20 16:17:24 +0200

    Check `hl` is set on Forum

--- forum/case_showthread.php
+++ forum/case_showthread.php
@@ -73,9 +73,11 @@ if(defined('_Forum')) {
           $edit = "";
         }
 
-        $ftxt = hl($getp['text'], $_GET['hl']);
-        if($_GET['hl']) $text = bbcode($ftxt['text']);
-        else $text = bbcode($getp['text']);
+        $ftxt = hl($getp['text'], isset($_GET['hl']) ? $_GET['hl'] : '');
+        if(isset($_GET['hl'])) 
+            $text = bbcode($ftxt['text']);
+        else 
+            $text = bbcode($getp['text']);
 
         if($chkMe == 4) $posted_ip = $getp['ip'];
         else $posted_ip = _logged;
