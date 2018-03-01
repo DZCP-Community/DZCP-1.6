849d45525b351678f78bd5e3f83bc9d756d37a40
Lucas <godkiller_nt@hammermaps.de>
2018-03-01 22:14:27 +0100

    Update Version to 1.6.0.5 Alpha

--- inc/_version.php
+++ inc/_version.php
@@ -4,7 +4,7 @@
  * http://www.dzcp.de
  */
 
-define('_version', '1.6.0.4');
-define('_release', '23.02.2018');
-define('_build', '1604.03.00');
+define('_version', '1.6.0.5');
+define('_release', '01.03.2018');
+define('_build', '1605.00.00');
 define('_edition', 'dev');
