58a9e8dce4b551312cff0a1e0d66eecb9fe954e2
Lucas <godkiller_nt@hammermaps.de>
2018-05-25 19:24:21 +0200

    Set language on register

--- user/case_register.php
+++ user/case_register.php
@@ -111,6 +111,7 @@ if(defined('_UserMenu')) {
                     `level`    = 1,
                     `pwd_md5`  = 0,
                     `dsgvo_lock` = 0,
+                    `language` = '".$_SESSION['language']."',
                     `time`     = ".time().",
                     `status`   = 1;");
 
