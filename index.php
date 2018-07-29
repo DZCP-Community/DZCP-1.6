e2ac8bea444ff24dff4556464a236cbfa541f9f5
Lucas <godkiller_nt@hammermaps.de>
2018-07-29 23:48:40 +0200

    Fix Instance of GUMP

--- index.php
+++ index.php
@@ -19,11 +19,14 @@ if (version_compare(phpversion(), '7.0', '<')) {
 if (file_exists(basePath . "/inc/mysql.php"))
     require_once(basePath . "/inc/mysql.php");
 
+include(basePath . '/vendor/autoload.php');
+use GUMP\GUMP;
+$gump = GUMP::get_instance();
+
 if (empty($sql_user) && empty($sql_pass) && empty($sql_db)) {
     header('Location: _installer/index.php');
 } else {
     $global_index = true;
-    include(basePath . '/vendor/autoload.php');
     include(basePath . "/inc/debugger.php");
     include(basePath . "/inc/config.php");
     include(basePath . "/inc/bbcode.php");
