a197e7e16e23b3500371388f5d6d31bef2eca3a0
Lucas <godkiller_nt@hammermaps.de>
2018-07-29 18:47:42 +0200

    Move function time_convert() to helper.php

--- teamspeak/index.php
+++ teamspeak/index.php
@@ -11,27 +11,12 @@ include("../inc/buffer.php");
 include(basePath . "/inc/debugger.php");
 include(basePath . "/inc/config.php");
 include(basePath . "/inc/bbcode.php");
+include(basePath . "/teamspeak/helper.php");
 
 ## SETTINGS ##
 $where = _site_teamspeak;
 $title = $pagetitle . " - " . $where . "";
 $dir = "teamspeak";
-
-function time_convert($time, $ms = false) {
-    if($ms) $time = $time / 1000;
-
-    $day = floor($time/86400);
-    $hours = floor(($time%86400)/3600);
-    $minutes = floor(($time%3600)/60);
-    $seconds = floor($time%60);
-
-    if($day>0) $time = $day."d ".$hours."h ".$minutes."m ".$seconds."s";
-    elseif($hours>0) $time = $hours."h ".$minutes."m ".$seconds."s";
-    elseif($minutes>0) $time = $minutes."m ".$seconds."s";
-    else $time = $seconds."s";
-
-    return $time;
-}
 
 ## SECTIONS ##
 if (fsockopen_support()) {
